1e39e577a308ceffb70c2bca2241c7f9950d94b2
GregN <grzegorz.niespodzianek@gmail.com>
2025-06-12 19:40:44 +0200

    Added grid with buttons

--- Calculator Mobile App/ContentView.swift
+++ Calculator Mobile App/ContentView.swift
@@ -9,19 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack{
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world bro!")
-            }
-            .padding()
-            Text("Hello, world bro!")
             
-        }
-        Text("Hello, world bro!")
-        }
+        HStack(spacing: 48) {
+            Grid (horizontalSpacing: 16, verticalSpacing: 16){
+                
+                GridRow {
+                    Button(action: {}) {
+                        Text("1").font(.largeTitle).frame(width: .infinity)
+                    }
+                    Button(action: {}) {
+                        Text("2").font(.largeTitle).frame(width: .infinity)
+                    }
+                    Button(action: {}) {
+                        Text("3").font(.largeTitle).frame(width: .infinity)
+                    }
+                }.gridCellColumns(3)
+                GridRow {
+                    Button(action: {}) {
+                        Text("4").font(.largeTitle).frame(width: .infinity)
+                    }
+                    Button(action: {}) {
+                        Text("5").font(.largeTitle).frame(width: .infinity)
+                    }
+                    Button(action: {}) {
+                        Text("6").font(.largeTitle).frame(width: .infinity)
+                    }
+                }.gridCellColumns(3)
+                GridRow {
+                    Button(action: {}) {
+                        Text("7").font(.largeTitle).frame(width: .infinity)
+                    }
+                    Button(action: {}) {
+                        Text("8").font(.largeTitle).frame(width: .infinity)
+                    }
+                    Button(action: {}) {
+                        Text("9").font(.largeTitle).frame(width: .infinity)
+                    }
+                }.gridCellColumns(3)
+            }.frame(width: .infinity)
+            VStack (spacing: 16){
+                Text("X").font(.largeTitle).frame(width: .infinity)
+                Text("-").font(.largeTitle).frame(width: .infinity)
+                Text("+").font(.largeTitle).frame(width: .infinity)
+                
+            }
+        }.frame(width: .infinity).padding()
+
+    }
+    
+    
         
     
 }
