ccf3be831a043ef9bfbd42ab9b784274e36cb267
wirelessguy123 <clockedoutgame@gmail.com>
2025-07-30 22:09:29 +0200

    Basic error handling

---
+++ Calculator Mobile App/CalculationInputView.swift
@@ -0,0 +1,18 @@
+//
+//  CalculationInputView.swift
+//  Calculator Mobile App
+//
+//  Created by WirelessGuy :) on 30/07/2025.
+//
+
+import SwiftUI
+
+struct CalculationInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalculationInputView()
+}
